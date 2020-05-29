a08d3e5320a89bc53ac84738c12ac31dd56b26f0
Samuel Silva <samuelrsilva@gmail.com>
2020-05-29 00:36:48 -0300

    Fix typo in gitignore; fix compilation in glCore

--- GTK3VisMol/VISMOL/glCore/setup.py
+++ GTK3VisMol/VISMOL/glCore/setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import numpy
 
-setup(ext_modules = cythonize("matrix_operations.pyx"))
-setup(ext_modules = cythonize("sphere_representation.pyx"))
+setup(ext_modules = cythonize("matrix_operations.pyx"), include_dirs=[numpy.get_include()])
+setup(ext_modules = cythonize("sphere_representation.pyx"), include_dirs=[numpy.get_include()])
 
